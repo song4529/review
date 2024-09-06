7f51f803cd5601d94889927e0b181314d7d543a6
song4529 <gusdn4529@gmail.com>
2024-09-06 14:17:15 +0900

    feat : ReviewBoard Controller 초기값 설정

---
+++ src/main/java/com/example/review/review_board/controller/ReviewBoardController.java
@@ -0,0 +1,11 @@
+package com.example.review.review_board.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/reviewBoards")
+@RestController
+@Validated
+public class ReviewBoardController {
+}
