3478edcbed5e9599d6fa016d8ae020cbc5acdc00
song4529 <gusdn4529@gmail.com>
2024-09-07 23:59:37 +0900

    feat : Comment Service 초기값 설정

---
+++ src/main/java/com/example/review/comment/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.example.review.comment.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class CommentService {
+}
